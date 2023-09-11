68aa30db8c8bcda712af86cdd92976219f5346dc
repeiakos1 <113334862+repeiakos1@users.noreply.github.com>
2023-09-11 14:01:40 +0200

    Add files via upload

---
+++ 3-4.ora/feladat_001.py
@@ -0,0 +1,23 @@
+# feladat_001
+
+"""
+
+Kérjük be a billentyűzetről a nevünket, és írassuk ki a képernyőre!
+A billentyűzetről mindig szöveget (string-et) olvassuk be!
+type(változó) 
+
+"""
+"""
+nev = input("Kérek egy nevet: ")
+print(f"A megadott név a következő: {nev}")
+"""
+
+vezeteknev = input("Mi a vezetékneved?")
+keresztnev = input("Kérnék egy keresztnevet")
+print(f"Szóval a neved: {vezeteknev} {keresztnev}")
+print(f"A vezeteknev változó típusa: {type(vezeteknev)}")
+print(f"A keresztnev változó típusa: {type(keresztnev)}")
+
+
+
+
